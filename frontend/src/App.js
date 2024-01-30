9d18d7eaad584fdd54b8052eff42561768507c61
unknown <vipinmathpal11@gmail.com>
2024-01-31 01:16:47 +0530

    add Router for Not found component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
-import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
-import './App.css';
-import Signup from "./components/Login&Signup/signup"
-import Login from "./components/Login&Signup/login"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Signup from "./components/Login&Signup/signup";
+import Login from "./components/Login&Signup/login";
+import NotFound from "./components/comman/NotFound"
 
 function App() {
   return (
-   <Router>
-     <Routes>
-    <Route  path="/Login" element={<Login></Login>}></Route>
-     <Route path="/Signup" element={<Signup></Signup>}></Route>
-     </Routes>
-          </Router>
-     
+    <Router>
+      <Routes>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+        <Route path="/Login" element={<Login></Login>}></Route>
+        <Route path="/Signup" element={<Signup></Signup>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
