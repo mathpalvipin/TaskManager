48ab076120cfcb615296ab558be93f7a54723956
unknown <vipinmathpal11@gmail.com>
2024-07-17 19:23:43 +0530

    fix color dynamic isseu

--- frontend/src/components/Task/Counter.js
+++ frontend/src/components/Task/Counter.js
@@ -1,5 +1,12 @@
 import { useSelector } from "react-redux";
+const colorVariants = {
+  blue: 'bg-blue-50 text-blue-800',
+  red: 'bg-red-50 text-red-800',
+  green: 'bg-green-50 text-green-800',
+  yellow: 'bg-yellow-50 text-yellow-800',
+  purple: 'bg-purple-50 text-purple-800',
 
+}
 const Counter = () => {
   const types = useSelector((state) => state.types);
   console.log(types);
@@ -8,7 +15,7 @@ const Counter = () => {
       {Object.keys(types).map((key) => (
         <div
           id={key}
-          className={`mb-1 mr-2 h-fit rounded-xl px-2 py-1 text-xs bg-${types[key].color}-50 text-${types[key].color}-800  tracking-wider`}
+          className={`mb-1 mr-2 h-fit rounded-xl px-2 py-1 text-xs  ${colorVariants[types[key].color]}   tracking-wider`}
         >
           {key} {types[key].count} 
         </div>
